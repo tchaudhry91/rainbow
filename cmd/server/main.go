12f33c643cfdb5ec723c69b0b32de8d148d64943
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2020-08-16 16:17:54 +0530

    Added graceful shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/go-kit/kit/log"
 	"github.com/tchaudhry91/rainbow/service"
 
 	"github.com/peterbourgon/ff/v3"
@@ -26,5 +31,31 @@ func main() {
 	svc := service.NewSHA256RainbowService(store)
 	server := service.NewRainbowHTTP(*listenAddr, svc)
 
-	server.Start()
+	shutdown := make(chan error, 1)
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	var err error
+
+	logger := log.NewJSONLogger(os.Stdout)
+	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+	logger = log.With(logger, "caller", log.DefaultCaller)
+
+	go func() {
+		logger.Log("msg", "Starting server..", "listenAddr", *listenAddr)
+		err = server.Start()
+		shutdown <- err
+	}()
+
+	select {
+	case signalKill := <-interrupt:
+		logger.Log("msg", fmt.Sprintf("Stopping Server: %s", signalKill.String()))
+	case err := <-shutdown:
+		logger.Log("error", err)
+	}
+
+	err = server.Shutdown(context.TODO())
+	if err != nil {
+		logger.Log("error", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/tchaudhry91/rainbow
 go 1.14
 
 require (
+	github.com/go-kit/kit v0.10.0
 	github.com/go-redis/redis/v8 v8.0.0-beta.4
 	github.com/gorilla/mux v1.7.4
 	github.com/peterbourgon/ff/v3 v3.0.0
